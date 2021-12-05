27098f4f4cf85b6ed3a6dc39f812a342b9ebcc51
Pinky <alejandro.hernandez@finerio.mx>
2021-12-05 14:52:59 -0600

    parallel disbursement and dto creation

--- src/main/java/com/example/clip/services/imp/TransactionServiceImp.java
+++ src/main/java/com/example/clip/services/imp/TransactionServiceImp.java
@@ -9,7 +9,6 @@ import com.example.clip.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
